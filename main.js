86718fd0cf8aa582192e87bb86f399184db3b449
Raposa_Dev <Kisukefelix@gmail.com>
2021-09-19 13:22:26 -0300

    criando contagem de participantes

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
 let inputName = document.getElementById("input-name");
 let inputAge = document.getElementById("input-age");
 let inputdate = document.getElementById("input-date");
+let participants = document.getElementById("count-participants");
+let countParticipants = 99;
 
 
 function ckeckDate() {
-    if(inputdate.value == "2021-09-08" && inputAge.value >= 18) {
-        alert(`
-        ${inputName.value} cadastro concluido!
-        Para o dia ${inputdate.value}
 
-        `);
-    } else if (inputAge.value === "" || inputName.value === "" || inputdate.value === ""){
-        alert(`
-            Preencha os campos!
-        `);
-    } else if(inputAge.value <= 17) {
-        alert(`
-        ${inputName.value} cadastro não pode ser concluido!
-         Menor de idade!
+    if (countParticipants < 100){
+        if(inputdate.value == "2021-09-08" && inputAge.value >= 18) {
+            alert(`
+            ${inputName.value} cadastro concluido!
+            Para o dia ${inputdate.value}
+            `);
 
-        `);
+            countParticipants++;
+        
+        } else if (inputAge.value === "" || inputName.value === "" || inputdate.value === ""){
+            alert(`
+                Preencha os campos!
+            `);
+        } else if(inputAge.value <= 17) {
+            alert(`
+            ${inputName.value} cadastro não pode ser concluido!
+            Menor de idade!
+            `);
+        } else {
+            alert(`
+                ${inputName.value}
+                Verifique os dados!
+            `);
+        };
+        
     } else {
-        alert(`
-            ${inputName.value}
-            Verifique os dados!
-        `);
+        alert("Número excedido de participantes no evento")
     };
+    
 
     inputName.value = "";
     inputAge.value = "";
+    participants.innerText = countParticipants;
 };
